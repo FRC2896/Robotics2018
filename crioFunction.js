14af2f98a7c12340ac48c709910a4b202e357a5a
Jordan <43512751+pirateio@users.noreply.github.com>
2018-09-22 23:58:21 -1000

    Update crioFunction.js

--- crioFunction.js
+++ crioFunction.js
@@ -6,7 +6,7 @@ function onAppLaunched() {
     alert("Welcome to LabView. Awaiting arguments.");
     sleep(1000);
     return 0;
-  } else if(clientr.app != "LabView.exe) {
+  } else if(clientr.app != "LabView.exe") {
     alert("LabView not found.");
     sleep(1000);
     exit(1);
