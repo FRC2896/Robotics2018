752e071dca29eb28cc96cf1fc404dc8de43f6dc4
Jordan <43512751+pirateio@users.noreply.github.com>
2018-09-22 23:47:53 -1000

    Update targetLoc.java

--- targetLoc.java
+++ targetLoc.java
@@ -1,1 +1,13 @@
-
+// Experimental! Testing with private Java compiler for future references
+public class robot extends dynamics {
+  const int x = target.x;
+  const int y = target.y;
+  const int z = target.z;
+  const boolean active = true;
+  double onwall = 3.147568;
+  
+  public static void main(String[] args) {
+    
+    return 0;
+  }
+}
