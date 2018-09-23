15a4ffb5e647140ad8ad923193fd33f71adf4b2b
Jordan <43512751+pirateio@users.noreply.github.com>
2018-09-22 22:43:58 -1000

    Update windowh.cpp

--- windowh.cpp
+++ windowh.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <Windows.h>
 
+using namespace std;
+
+int main() {
+  
+  HWND hwnd = findWindow(NULL, "LabView.exe");
+  if(hwnd == NULL) {
+    cout << "LabView was not found. Please make sure there is an instance running." << endl;
+    sleep(1);
+    exit;
+  } else if (hwnd == "LabView.exe") {
+    cout << "LabView instance found!" << endl;
+    sleep(1);
+    exit;
+  }
+  
+  return 0;
+}
