441d1493ffe6cf34c52beaf7c3fb1b7d64d813c3
Jordan <43512751+pirateio@users.noreply.github.com>
2018-09-22 23:57:58 -1000

    Update crioFunction.js

--- crioFunction.js
+++ crioFunction.js
@@ -1,1 +1,22 @@
+// Testing these on private localhost provider! Experimental
+
+//#CLIENTSIDE
+function onAppLaunched() {
+  if(clientr.app = "LabView.exe") {
+    alert("Welcome to LabView. Awaiting arguments.");
+    sleep(1000);
+    return 0;
+  } else if(clientr.app != "LabView.exe) {
+    alert("LabView not found.");
+    sleep(1000);
+    exit(1);
+    return 0;;
+  }
+
+function onKeyPressed("scan", "key", "keypressed") {
+  if(keypress == "LShift") {
+    prompt.GUI("-robot%20ExtraOptions");
+    alert("Side-options loaded.");
+    return 0;
+  }
 
